d373d0db939ca2d6f6b05679b2287df4916dbab1
hanrx <1170807870@qq.com>
2020-03-18 17:07:00 +0800

    添加 依赖 Swagger支持

--- src/main/java/com/iafoot/dbandcache/DbandcacheApplication.java
+++ src/main/java/com/iafoot/dbandcache/DbandcacheApplication.java
@@ -2,8 +2,10 @@ package com.iafoot.dbandcache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DbandcacheApplication {
 
     public static void main(String[] args) {
