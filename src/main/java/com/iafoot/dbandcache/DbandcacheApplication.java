568b8588e99b81dab103db3ddf2570fa25d5a951
hanrx <1170807870@qq.com>
2020-03-18 16:04:18 +0800

    Initial commit

---
+++ src/main/java/com/iafoot/dbandcache/DbandcacheApplication.java
@@ -0,0 +1,13 @@
+package com.iafoot.dbandcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbandcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbandcacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iafoot/dbandcache/DbandcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iafoot.dbandcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbandcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
