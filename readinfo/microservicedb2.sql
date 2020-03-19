a2ee95f24e9c5519d877f9ce28ab16ebb788d8f6
hanrx <1170807870@qq.com>
2020-03-19 12:05:50 +0800

    添加数据库脚本

---
+++ readinfo/microservicedb2.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : localhost:3306
+ Source Schema         : microservicedb2
+
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
+
+ Date: 19/03/2020 12:03:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_car
+-- ----------------------------
+DROP TABLE IF EXISTS `t_car`;
+CREATE TABLE `t_car`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '车名',
+  `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '车主',
+  `owner` bigint(0) NOT NULL COMMENT '车主',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_car
+-- ----------------------------
+INSERT INTO `t_car` VALUES (1, '奥迪', 'red', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
